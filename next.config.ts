f27775ba274832158efdcd76298063723adce2bb
kimJina2 <fuzkjn@gmail.com>
2026-03-16 01:45:45 +0900

    Qwen 3.5 vision 영상분석 구현 세팅 추가(next.config.ts, pakage)

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["nodemailer"],
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "sharp$": false,
+      "onnxruntime-node$": false,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
